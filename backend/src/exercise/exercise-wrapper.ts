3acd1c64c113ad8e2e0e872b1d2da88231a78ac2
Nils <45318774+Nils1729@users.noreply.github.com>
2023-06-12 13:57:29 +0200

    Fix rebase

--- backend/src/exercise/exercise-wrapper.ts
+++ backend/src/exercise/exercise-wrapper.ts
@@ -14,6 +14,7 @@ import {
     ReducerError,
     validateExerciseAction,
     validateExerciseState,
+    patientTick
 } from 'digital-fuesim-manv-shared';
 import type { EntityManager } from 'typeorm';
 import { LessThan } from 'typeorm';
