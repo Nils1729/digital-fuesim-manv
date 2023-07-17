045981376aece88c2729e856939383e4f647a7c1
Nils <45318774+Nils1729@users.noreply.github.com>
2023-07-17 13:03:22 +0200

    Improve benchmark

--- nh-thesis-benchmarks/cypress/partial-state-fractions.cy.ts
+++ nh-thesis-benchmarks/cypress/partial-state-fractions.cy.ts
@@ -3,46 +3,17 @@ import { ExerciseConfiguration } from 'digital-fuesim-manv-shared';
 describe('execute some patient ticks', () => {
     const timings: any = [];
 
-    const configs: ExerciseConfiguration['standInConfig'][] = [
-        {
-            useStandIns: false,
-            preComputation: { patients: false, standIns: false },
-        },
-        {
-            useStandIns: false,
-            preComputation: { patients: false, standIns: true },
-        },
-        {
-            useStandIns: false,
-            preComputation: { patients: true, standIns: false },
-        },
-        {
-            useStandIns: false,
-            preComputation: { patients: true, standIns: true },
-        },
-        {
-            useStandIns: true,
-            preComputation: { patients: false, standIns: true },
-            holdInterval: 1_000_000_000,
-            updateInterval: 1_000_000_000,
-        },
-        {
-            useStandIns: true,
-            preComputation: { patients: true, standIns: true },
-            holdInterval: 1_000_000_000,
-            updateInterval: 1_000_000_000,
-        },
-    ];
     const settings: {
         pas: number;
         loaded_pas: number;
+        fraction_pas: number;
         ticks: number;
         run: number;
         config: ExerciseConfiguration['standInConfig'];
     }[] = [];
-    const pa_counts = [20];
-    const pa_fractions = [0.25];
-    const tick_counts = [10];
+    const pa_counts = [0, 5, 10, 15, 20, 30, 40, 50];
+    const pa_fractions = [0, 0.2, 0.4, 0.6, 0.8, 1];
+    const tick_counts = [300];
     const repetitions = 1;
     for (let i = 0; i < repetitions; i++) {
         tick_counts.forEach((ticks) =>
@@ -50,7 +21,8 @@ describe('execute some patient ticks', () => {
                 pa_fractions.forEach((paf) =>
                     settings.push({
                         pas,
-                        loaded_pas: pas * paf,
+                        loaded_pas: Math.floor(pas * paf),
+                        fraction_pas: paf,
                         ticks,
                         run: i,
                         config: {
@@ -71,13 +43,12 @@ describe('execute some patient ticks', () => {
         cy.get('span:contains(0:00:10)', { timeout: 15_000 });
     });
 
-    settings.forEach(({ pas, loaded_pas, ticks, run, config }) => {
+    settings.forEach(({ pas, loaded_pas, fraction_pas, ticks, run, config }) => {
         it(`records ${ticks} ticks with ${pas} regions, ${loaded_pas} loaded (${
             run + 1
         } / ${repetitions})`, () => {
             cy.preparePartialScenario(pas, loaded_pas, config);
             cy.get('[data-cy=confirmationModalOkButton]').click();
-            // cy.get('span:contains(0:00:01)', { timeout: 5_000 });
             cy.get('[data-cy=trainerToolbarExecutionButton]').click();
             cy.get('button:contains(Simulationsübersicht)').click();
             cy.wait(5_000);
@@ -92,6 +63,7 @@ describe('execute some patient ticks', () => {
                     settings: {
                         pas,
                         loaded_pas,
+                        fraction_pas,
                         ticks,
                         run,
                         config,
