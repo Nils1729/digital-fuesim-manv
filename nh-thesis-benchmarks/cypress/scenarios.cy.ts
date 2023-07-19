bd613ceeb9bd7ea2eb0aaf76195f2f284d64f559
Nils <45318774+Nils1729@users.noreply.github.com>
2023-07-19 16:24:39 +0200

    Add control to benchmark

--- nh-thesis-benchmarks/cypress/scenarios.cy.ts
+++ nh-thesis-benchmarks/cypress/scenarios.cy.ts
@@ -1,77 +1,93 @@
 import cypress from 'cypress';
 import { ExerciseConfiguration } from 'digital-fuesim-manv-shared';
 
-describe(
-    'execute scenarios',
+const implementations: { name: string; host: string; root: string }[] = [
     {
-        baseUrl: 'http://localhost:8001',
+        name: 'test',
+        host: 'http://localhost:8001',
+        root: 'scenarios/thesis',
     },
-    () => {
-        const timings: any = [];
-        const root = 'scenarios/thesis';
-        const settings: {
-            filename?: string;
-            pas: number;
-            loaded_pas: number;
-            fraction_pas: number;
-            ticks: number;
-            run: number;
-            config: ExerciseConfiguration['standInConfig'];
-        }[] = require(`../${root}/settings.json`);
+    {
+        name: 'control',
+        host: 'http://localhost:8002',
+        root: 'scenarios/main',
+    },
+];
 
-        settings.forEach(
-            ({
-                filename,
-                pas,
-                loaded_pas,
-                fraction_pas,
-                ticks,
-                run,
-                config,
-            }) => {
-                it(`records ${ticks} ticks with ${pas} regions, ${loaded_pas} loaded (${run})`, () => {
-                    cy.visit('/').reload(true);
-                    cy.readFile(`${root}/${filename}`).then((body) => {
-                        cy.createExerciseProduction(
-                            body
-                        ).joinExerciseAsTrainer();
-                        cy.log('start an exercise')
-                            .get('[data-cy=trainerToolbarStartButton]', {
-                                timeout: 10_000,
-                            })
-                            .click();
-                        cy.get('[data-cy=confirmationModalOkButton]').click();
-                        cy.wait(5_000);
+implementations.forEach(({ host, name, root }) => {
+    describe(
+        `execute scenarios for ${name}`,
+        {
+            baseUrl: host,
+        },
+        () => {
+            const timings: any = [];
+            const settings: {
+                filename?: string;
+                pas: number;
+                loaded_pas: number;
+                fraction_pas: number;
+                ticks: number;
+                run: number;
+                config: ExerciseConfiguration['standInConfig'];
+            }[] = require(`../${root}/settings.json`);
 
-                        cy.actionTiming().then((a) => a.clear());
+            settings.forEach(
+                ({
+                    filename,
+                    pas,
+                    loaded_pas,
+                    fraction_pas,
+                    ticks,
+                    run,
+                    config,
+                }) => {
+                    it(`records ${ticks} ticks with ${pas} regions, ${loaded_pas} loaded (${run})`, () => {
+                        cy.visit('/').reload(true);
+                        cy.readFile(`${root}/${filename}`).then((body) => {
+                            cy.createExerciseProduction(
+                                body
+                            ).joinExerciseAsTrainer();
+                            cy.log('start an exercise')
+                                .get('[data-cy=trainerToolbarStartButton]', {
+                                    timeout: 10_000,
+                                })
+                                .click();
+                            cy.get(
+                                '[data-cy=confirmationModalOkButton]'
+                            ).click();
+                            cy.wait(5_000);
 
-                        cy.actionTiming()
-                            .its('sampleCount', { timeout: ticks * 1_500 })
-                            .should('be.at.least', ticks + 1);
-                        cy.actionTiming().then((a) => {
-                            timings.push({
-                                settings: {
-                                    pas,
-                                    loaded_pas,
-                                    fraction_pas,
-                                    ticks,
-                                    run,
-                                    config,
-                                },
-                                stats: a.series_stats(ticks),
-                                raw: a.series(ticks),
+                            cy.actionTiming().then((a) => a.clear());
+
+                            cy.actionTiming()
+                                .its('sampleCount', { timeout: ticks * 1_500 })
+                                .should('be.at.least', ticks + 1);
+                            cy.actionTiming().then((a) => {
+                                timings.push({
+                                    settings: {
+                                        pas,
+                                        loaded_pas,
+                                        fraction_pas,
+                                        ticks,
+                                        run,
+                                        config,
+                                    },
+                                    stats: a.series_stats(ticks),
+                                    raw: a.series(ticks),
+                                });
                             });
                         });
                     });
-                });
-            }
-        );
-
-        it('saves recorded timings', () => {
-            cy.writeFile(
-                `data/${root}/${new Date().toISOString()}.json`,
-                timings
+                }
             );
-        });
-    }
-);
+
+            it('saves recorded timings', () => {
+                cy.writeFile(
+                    `data/${root}/${new Date().toISOString()}.json`,
+                    timings
+                );
+            });
+        }
+    );
+});
